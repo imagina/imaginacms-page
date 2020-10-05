56ac0e8c9a7b127da6d1f4debdde3aacacef2e14
JCEC007 <jcec007@gmail.com>
2020-10-05 15:37:03 -0500

    +fix: validate seeder if the home page already exist. +update: change seed template from "home" to "default"

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -21,21 +21,26 @@ class PageDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-
+  
+      $page = $this->page->findByAttributes(["is_home",1]);
+      
+      if(!isset($page->id)){
         $data = [
-            'template' => 'home',
-            'is_home' => 1,
-            'en' => [
-                'title' => 'Home page',
-                'slug' => 'home',
-                'body' => '<p><strong>You made it!</strong></p>
+          'template' => 'default',
+          'is_home' => 1,
+          'en' => [
+            'title' => 'Home page',
+            'slug' => 'home',
+            'body' => '<p><strong>You made it!</strong></p>
 <p>You&#39;ve installed AsgardCMS and are ready to proceed to the <a href="/en/backend">administration area</a>.</p>
 <h2>What&#39;s next ?</h2>
 <p>Learn how you can develop modules for AsgardCMS by reading our <a href="https://github.com/AsgardCms/Documentation">documentation</a>.</p>
 ',
-                'meta_title' => 'Home page',
-            ],
+            'meta_title' => 'Home page',
+          ],
         ];
         $this->page->create($data);
+      }
+      
     }
 }
