8583e41275af1b2c1badcd61eebc4481f04a354c
JCEC007 <jcec007@gmail.com>
2020-10-05 20:37:55 -0500

    +add: seed to OurHistory/Nosotros and Contact/Contacto

--- Database/Seeders/PageDatabaseSeeder.php
+++ Database/Seeders/PageDatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Modules\Page\Database\Seeders;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Modules\Page\Entities\PageTranslation;
 use Modules\Page\Repositories\PageRepository;
 
 class PageDatabaseSeeder extends Seeder
@@ -22,8 +23,9 @@ class PageDatabaseSeeder extends Seeder
     {
         Model::unguard();
   
+      //Seed Home Page/Inicio
       $page = $this->page->findByAttributes(["is_home" => 1]);
-      
+  
       if(!isset($page->id)){
         $data = [
           'template' => 'default',
@@ -44,6 +46,52 @@ class PageDatabaseSeeder extends Seeder
             'body' => '<p><strong>Lo lograste!</strong></p>
 <p>has instalado el ImaginaCMS, vé ahora al <a href="/iadmin">área de administración</a>.</p>',
             'meta_title' => 'Página de Inicio',
+          ],
+        ];
+        $this->page->create($data);
+      }
+  
+      //Seed Our History/Nosotros
+      $history = PageTranslation::where("slug","history")->first();
+  
+      if(!isset($history->id)){
+        $data = [
+          'template' => 'default',
+          'is_home' => 0,
+          'en' => [
+            'title' => 'Our History',
+            'slug' => 'history',
+            'body' => '<p>Our history.</p>',
+            'meta_title' => 'Our History',
+          ],
+          'es' => [
+            'title' => 'Nosotros',
+            'slug' => 'nosotros',
+            'body' => '<p>Nosotros.</p>',
+            'meta_title' => 'Nosotros',
+          ],
+        ];
+        $this->page->create($data);
+      }
+  
+      //Seed Our Contact/Contacto
+      $contact = PageTranslation::where("slug","contact")->first();
+  
+      if(!isset($contact->id)){
+        $data = [
+          'template' => 'default',
+          'is_home' => 0,
+          'en' => [
+            'title' => 'Contact',
+            'slug' => 'contact',
+            'body' => '<p>Contact.</p>',
+            'meta_title' => 'Contact',
+          ],
+          'es' => [
+            'title' => 'Contacto',
+            'slug' => 'contacto',
+            'body' => '<p>Contacto.</p>',
+            'meta_title' => 'Contacto',
           ],
         ];
         $this->page->create($data);
