2d9852b1d2b885995bc8099b8a2264c4d9c0fedb
Juan Escalante <jcec007@gmail.com>
2022-04-20 09:45:57 -0500

    +Add: Page ENtity | added url attribute +Add: EloquentPageRepository | added filter by tags and the body +Fix: EloquentPageRepository | validate index-all permission

--- Repositories/Eloquent/EloquentPageRepository.php
+++ Repositories/Eloquent/EloquentPageRepository.php
@@ -125,7 +125,7 @@ class EloquentPageRepository extends EloquentBaseRepository implements PageRepos
       })->with('translations')->first();
     }
 
-    return $this->model->where('slug', $slug)->where('locale', $locale)->first();
+    return $this->model->where('slug', $slug)->where('locale', $locale)->where('type', '!=', 'cms')->first();
   }
 
   /**
@@ -274,10 +274,13 @@ class EloquentPageRepository extends EloquentBaseRepository implements PageRepos
       if (isset($filter->search) && $filter->search) {
         //find search in columns
         $term = $filter->search;
-        $query->where(function ($subQuery) use ($term) {
-          $subQuery->whereHas('translations', function ($q) use ($term) {
-            $q->where('title', 'LIKE', "%{$term}%");
-            $q->orWhere('slug', 'LIKE', "%{$term}%");
+        $query->where(function ($query) use ($term) {
+          $query->whereHas('translations', function ($query) use ($term) {
+            $query->where('title', 'LIKE', "%{$term}%");
+            $query->where('body', 'LIKE', "%{$term}%");
+            $query->orWhere('slug', 'LIKE', "%{$term}%");
+          })->orWhere(function ($query) use ($term){
+            $query->whereTag($term,'name');
           })
             ->orWhere('id', $term);
         });
@@ -307,12 +310,13 @@ class EloquentPageRepository extends EloquentBaseRepository implements PageRepos
       $query->select($params->fields);
 
     //Validate index-all permission
-    if (!isset($params->allowIndexAll) && !in_array('page.pages.index-all', $params->permissions))
+    if (!isset($params->allowIndexAll) && (!isset($params->permissions) || !in_array('page.pages.index-all', $params->permissions)))
       $query->whereNull('type');
 
+  //  dd($query->toSql(), $query->getBindings(), $params);
     /*== REQUEST ==*/
     if (isset($params->page) && $params->page) {
-      return $query->paginate($params->take);
+      return $query->paginate($params->take, ['*'], null, $params->page);
     } else {
       $params->take ? $query->take($params->take) : false;//Take
       return $query->get();
