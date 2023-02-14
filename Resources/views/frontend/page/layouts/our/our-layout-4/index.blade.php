5ba4998d7993b7b80f54598a9d556f7367bacc4f
Cristian Marin <marin101998@gmail.com>
2023-02-14 18:09:01 -0500

    +fix | padding content layout 4 page our

--- Resources/views/frontend/page/layouts/our/our-layout-4/index.blade.php
+++ Resources/views/frontend/page/layouts/our/our-layout-4/index.blade.php
@@ -5,7 +5,9 @@
     class="page-banner banner-breadcrumb-category position-relative page-contact">
     <div class="position-absolute h-100 w-100 content-title">
       <div class="container d-flex flex-column align-items-center w-100 h-100 justify-content-center">
-        {{$page->title}}
+        <div class="">
+          {{$page->title}}
+        </div>
         @include('page::frontend.partials.breadcrumb')
       </div>
     </div>
@@ -17,13 +19,15 @@
       <div class="pb-5 pt-5" style="background-color: var(--primary)"></div>
     @endif
   </div>
-  <div class="content-page">
+  <div class="content-page pt-5">
     <div class="container">
       @if (isset($page) && empty($page->mainimage) && strpos($page->mediaFiles()->mainimage->extraLargeThumb, 'default.jpg') == false)
         <x-media::single-image :title="$page->title" :isMedia="true" width="100%" :mediaFiles="$page->mediaFiles()"
                                zone="mainimage"/>
       @endif
-      {!! $page->body !!}
+      <div class="description-page pt-5">
+        {!! $page->body !!}
+      </div>
     </div>
     <div class="gallery-section py-3">
       <x-media::gallery :mediaFiles="$page->mediaFiles()"
