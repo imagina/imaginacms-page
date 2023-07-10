b1654342ae158617c3036bafd4042bce291c4e86
William Verde <williamverde10@gmail.com>
2023-07-10 15:51:10 -0400

    Added: logs in pages ai

--- Services/PageContentAi.php
+++ Services/PageContentAi.php
@@ -9,6 +9,7 @@ use Modules\Isite\Services\AiService;
 class PageContentAi
 {
   public $aiService;
+  private $log = "Page: Services|PageContentAi|";
 
   function __construct()
   {
@@ -17,12 +18,15 @@ class PageContentAi
 
   public function getPages($quantity = 2)
   {
+    \Log::info($this->log."getPages|INIT");
+
     //instance the prompt to generate the posts
     $prompt = "Contenido extenso para una pagina WEB con los siguientes atributos ";
     //Instance attributes
     $prompt .= $this->aiService->getStandardPrompts(["title", "body", "slug"]);
     //Call IA Service
     $response = $this->aiService->getContent($prompt, $quantity);
+    \Log::info($this->log."getPages|END");
     //Return response
     return $response;
   }
