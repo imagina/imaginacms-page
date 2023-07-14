4ca7821e4500ec99a9f2a5ab6379dc3605cc4167
William Verde <williamverde10@gmail.com>
2023-07-14 11:28:50 -0400

    Added: Processes to page content ai

--- Services/PageContentAi.php
+++ Services/PageContentAi.php
@@ -10,18 +10,23 @@ class PageContentAi
 {
   public $aiService;
   private $log = "Page: Services|PageContentAi|";
+  private $maxAttempts = 3;
+  private $pageRepository;
 
   function __construct()
   {
     $this->aiService = new AiService();
+    $this->pageRepository = app("Modules\Page\Repositories\PageRepository");
   }
 
-  public function getPages($quantity = 2)
+  public function getPages($quantity = 2,$page)
   {
     \Log::info($this->log."getPages|INIT");
 
     //instance the prompt to generate the posts
-    $prompt = "Contenido extenso para una pagina WEB con los siguientes atributos ";
+    $prompt = "Contenido extenso para una pagina WEB de:".$page->title.", con los siguientes atributos ";
+    //\Log::info($this->log."getPages|prompt: ".$prompt);
+    
     //Instance attributes
     $prompt .= $this->aiService->getStandardPrompts(["title", "body", "slug"]);
     //Call IA Service
@@ -30,4 +35,100 @@ class PageContentAi
     //Return response
     return $response;
   }
+
+  /**
+  * Principal
+  */
+  public function startProcesses()
+  {
+
+    \Log::info($this->log."startProcesses");
+
+    //Only pages to update
+    $pages = $this->getPagesToUpdate();
+
+    //Get new information to each page
+    foreach ($pages as $key => $page) {
+
+      $newData = $this->getNewData($page);
+      
+      if(!is_null($newData)){
+        $this->updatePage($page,$newData[0]);
+      }
+
+    }
+
+  }
+
+  /**
+  * Get Pages to Update
+  */
+  public function getPagesToUpdate()
+  {
+
+    $params = [
+      "filter" => [
+        'systemName' => ['us','contact'] //Only this pages
+      ],
+    ];
+      
+    $pages = $this->pageRepository->getItemsBy(json_decode(json_encode($params)));
+
+    return $pages;
+       
+  }
+
+  /**
+  * Get the New Data
+  */
+  public function getNewData($page)
+  {
+    
+    $newData = null;
+
+    $attempts = 0;
+    do {
+      \Log::info($this->log."getNewData|Attempt:".($attempts+1));
+      $newData = $this->getPages(1,$page);
+      if(is_null($newData)){
+        $attempts++;
+      }else{
+        break;
+      }
+    }while($attempts < $this->maxAttempts);
+
+    return $newData;
+  }
+
+  /**
+  * Update new data to pages
+  */
+  public function updatePage($page,$data)
+  {
+
+    \Log::info($this->log."updatePage|".$page->title);
+
+    //A veces como que no llegaba el index 
+    if(isset($data['es'])){
+      
+      $dataToUpdate = [
+        'es' => [
+          'body' => $data['es']['body']
+        ],
+        'en' => [
+          'body' => $data['en']['body']
+        ]
+      ];
+
+      $page->update($dataToUpdate);
+
+      //TODO
+      //Proceso para sustituir la imagen
+
+    }
+
+  }
+
+
+
 }
