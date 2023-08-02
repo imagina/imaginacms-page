529ae457c03cd8810978815b94fbb6026231e609
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2023-08-02 15:22:22 -0500

    V8.x (#27)
    * +fix | layouts page with params is internal true
    
    * +fix | layout 5 contact validate if isset breadcrumb and add new breadcrumb section
    
    * +add | filter internal in crud field layout pages
    
    * +fix | decode params in query if exist page in seeder CreatePagesFromModulesTableSeeder.php

--- Database/Seeders/CreatePagesFromModulesTableSeeder.php
+++ Database/Seeders/CreatePagesFromModulesTableSeeder.php
@@ -10,74 +10,73 @@ use Modules\Page\Repositories\PageRepository;
 class CreatePagesFromModulesTableSeeder extends Seeder
 {
 
-    private $page;
-    
-    public function __construct(PageRepository $page)
-    {
-      $this->page = $page;
-    }
+  private $page;
 
-   
-    public function run()
-    {
+  public function __construct(PageRepository $page)
+  {
+    $this->page = $page;
+  }
 
-        Model::unguard();
 
-        \Log::info("----------------------------------------------------------");
-        \Log::info("Seeder - CreatePagesFromModules");
-        \Log::info("----------------------------------------------------------");
-        
-        $modules = app('modules')->allEnabled();
-    
-        foreach ($modules as $module) {
-            $lowercaseModule = strtolower($module->get('name'));
-            //\Log::info("Module: ".$lowercaseModule);
+  public function run()
+  {
 
-            $pagesBase = config("asgard.".$lowercaseModule.".config.pagesBase");
-            if(!is_null($pagesBase)){
-                \Log::info("Creating pages to Module: ".$lowercaseModule);
-                foreach ($pagesBase as $key => $page) {
-                  
-                    $params = ["filter" => ["field" => 'system_name']];
-                    $existPage = $this->page->getItem($page['system_name'], $params);
-                    
-                    if (!$existPage) {
-                        $this->createPage($page);
-                    }
-                }
-            }
+    Model::unguard();
 
+    \Log::info("----------------------------------------------------------");
+    \Log::info("Seeder - CreatePagesFromModules");
+    \Log::info("----------------------------------------------------------");
+
+    $modules = app('modules')->allEnabled();
+
+    foreach ($modules as $module) {
+      $lowercaseModule = strtolower($module->get('name'));
+      //\Log::info("Module: ".$lowercaseModule);
+
+      $pagesBase = config("asgard." . $lowercaseModule . ".config.pagesBase");
+
+      if (!is_null($pagesBase)) {
+        \Log::info("Creating pages to Module: " . $lowercaseModule);
+        foreach ($pagesBase as $key => $page) {
+          $params = ["filter" => ["field" => 'system_name']];
+          $existPage = $this->page->getItem($page['system_name'], json_decode(json_encode($params)));
+          if (!$existPage) {
+            $this->createPage($page);
+          }
         }
+      }
 
     }
 
-    public function createPage(array $data)
-    {
+  }
 
-        $dataToCreate = [
-            'template' => $data['template'] ?? 'default',
-            'is_home' => $data['is_home'] ?? 0,
-            'system_name' => $data['system_name'],
-            'type' => $data['type'] ?? null,
-            'en' => [
-                'title' => $data['en']['title'] ?? null,
-                'slug' => $data['en']['slug'] ?? null,
-                'body' => $data['en']['body'] ?? '<p>Page</p>',
-                'meta_title' =>  $data['en']['meta_title'] ?? null
-            ],
-            'es' => [
-                'title' => $data['es']['title'] ?? null,
-                'slug' => $data['es']['slug'] ?? null,
-                'body' => $data['es']['body'] ?? '<p>Pagina</p>',
-                'meta_title' =>  $data['es']['meta_title'] ?? null,
-            ],
-            'options' => $data['options'] ?? null
-        ];
+  public function createPage(array $data)
+  {
 
-        //\Log::info("Data to Create Page: ".\json_encode($dataToCreate));
+    $dataToCreate = [
+      'template' => $data['template'] ?? 'default',
+      'is_home' => $data['is_home'] ?? 0,
+      'system_name' => $data['system_name'],
+      'type' => $data['type'] ?? null,
+      'en' => [
+        'title' => $data['en']['title'] ?? null,
+        'slug' => $data['en']['slug'] ?? null,
+        'body' => $data['en']['body'] ?? '<p>Page</p>',
+        'meta_title' => $data['en']['meta_title'] ?? null
+      ],
+      'es' => [
+        'title' => $data['es']['title'] ?? null,
+        'slug' => $data['es']['slug'] ?? null,
+        'body' => $data['es']['body'] ?? '<p>Pagina</p>',
+        'meta_title' => $data['es']['meta_title'] ?? null,
+      ],
+      'options' => $data['options'] ?? null
+    ];
 
-        $pageCreated = $this->page->create($dataToCreate); 
+    //\Log::info("Data to Create Page: ".\json_encode($dataToCreate));
 
-    }
+    $pageCreated = $this->page->create($dataToCreate);
+
+  }
 
 }
