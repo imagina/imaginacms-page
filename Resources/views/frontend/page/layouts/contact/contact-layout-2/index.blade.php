babc6e041e4e7edf813843ce4bfb947412ce00f6
Cristian Marin <marin101998@gmail.com>
2023-09-05 11:54:11 -0500

    +fix | layout 2 contact position body and breadcrumb by default

--- Resources/views/frontend/page/layouts/contact/contact-layout-2/index.blade.php
+++ Resources/views/frontend/page/layouts/contact/contact-layout-2/index.blade.php
@@ -5,25 +5,23 @@
         <h1 class="text-white text-uppercase title-page">
           {{$page->title}}
         </h1>
-        @include('page::frontend.partials.breadcrumb')
       </div>
     </div>
     @if (isset($page) && empty($page->breadcrumb) && strpos($page->mediaFiles()->breadcrumbimage->extraLargeThumb, 'default.jpg') == false)
       <x-media::single-image :title="$page->title" :isMedia="true" width="100%" :mediaFiles="$page->mediaFiles()"
                              zone="breadcrumbimage"/>
     @else
-      <img class="img-fluid w-100 h-100" src="{{ Theme::url('img/banner-page.jpg') }}" alt="img-{{$page->title}}">
+      <div class="pb-5 pt-5" style="background-color: var(--primary)"></div>
     @endif
   </div>
   <div class="page-content">
-    <div class="container pt-5 py-3 py-md-5">
+    <div class="container py-3 py-md-5">
+      @include('page::frontend.partials.breadcrumb')
       <div class="row">
-        <div class="col-12">
-          <div class="page-body mb-5">
+        <div class="col-lg-6 mb-md-5">
+          <div class="page-body mb-2">
             {!! $page->body !!}
           </div>
-        </div>
-        <div class="col-lg-6 mb-md-5">
           @php
             $formRepository = app("Modules\Iforms\Repositories\FormRepository");
             $params = [
