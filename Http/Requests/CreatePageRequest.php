a4fa7f36eb881b5e97deb818d42c7db78fcae8ed
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2024-10-24 16:15:35 -0500

    + fix | v10 08-10-24 added validation for the page slug column (#50)
    * + fix | v10 08-10-24 added validation for the page slug column
    
    * + fix | v10 22-10-24 last version v8

--- Http/Requests/CreatePageRequest.php
+++ Http/Requests/CreatePageRequest.php
@@ -6,42 +6,42 @@ use Modules\Core\Internationalisation\BaseFormRequest;
 
 class CreatePageRequest extends BaseFormRequest
 {
-    protected $translationsAttributesKey = 'page::pages.validation.attributes';
+  protected $translationsAttributesKey = 'page::pages.validation.attributes';
 
-    public function rules()
-    {
-        return [
-            'template' => 'required',
-        ];
-    }
+  public function rules()
+  {
+    return [
+      'template' => 'required',
+    ];
+  }
 
-    public function translationRules()
-    {
-        return [
-            'title' => 'required',
-            'slug' => 'required',
-        ];
-    }
+  public function translationRules()
+  {
+    return [
+      'title' => 'required',
+      'slug' => ['required', 'alpha_dash:ascii'],
+    ];
+  }
 
-    public function authorize()
-    {
-        return true;
-    }
+  public function authorize()
+  {
+    return true;
+  }
 
-    public function messages()
-    {
-        return [
-            'template.required' => trans('page::messages.template is required'),
-            'is_home.unique' => trans('page::messages.only one homepage allowed'),
-        ];
-    }
+  public function messages()
+  {
+    return [
+      'template.required' => trans('page::messages.template is required'),
+      'is_home.unique' => trans('page::messages.only one homepage allowed'),
+    ];
+  }
 
-    public function translationMessages()
-    {
-        return [
-            'title.required' => trans('page::messages.title is required'),
-            'slug.required' => trans('page::messages.slug is required'),
-            'body.required' => trans('page::messages.body is required'),
-        ];
-    }
+  public function translationMessages()
+  {
+    return [
+      'title.required' => trans('page::messages.title is required'),
+      'slug.required' => trans('page::messages.slug is required'),
+      'body.required' => trans('page::messages.body is required'),
+    ];
+  }
 }
